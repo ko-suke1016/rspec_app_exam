8aee24b07806d9e70876e40ccfed5013fd917809
ko-suke1016 <program8910@gmail.com>
2021-02-07 14:21:03 +0900

    Fix FactoryBotのtraitを導入しリファクタリング

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -83,7 +83,7 @@ RSpec.describe 'Task', type: :system do
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
         # TODO: FactoryBotのtraitを利用してください
         project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id, status: :done, completion_date: Time.current.yesterday)
+        task = FactoryBot.create(:task, :project_date, project_id: project.id)
         visit edit_project_task_path(project, task)
         select 'todo', from: 'Status'
         click_button 'Update Task'
